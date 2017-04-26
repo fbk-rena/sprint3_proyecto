90aafb7fadb1a8c27792566bb0cf6cb43a3ded73
mireyasantiago <mireyasantiago105@gmail.com>
2017-04-26 13:51:10 -0500

    cambios

---
+++ header js/header.js
@@ -0,0 +1,7 @@
+var cabezal = document.getElementsByTagName('header');
+var boton = document.getElementById('botonSignUp');
+
+function cambiarColor () {
+  cabezal[0].style.background-color='white';
+  boton.style.display='inline-block';
+}
